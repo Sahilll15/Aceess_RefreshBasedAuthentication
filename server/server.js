554bcb60c4e875af5b7ffd3cd5f43357a98f563b
Sahilll15 <sahilchalke1011@gmail.com>
2024-11-09 16:15:32 +0530

    initi

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB', err));
+
+app.use('/api', authRoutes);
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
